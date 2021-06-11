418d36d60dd251a13ee969065bcb5b3bd8c9d380
Alexandre V <algator67@hotmail.fr>
2021-06-11 14:22:20 +0200

    First animation of button
    the buttons aren't yet properlyu linked

---
+++ LP2B_project/Assets/Menu/Scripts/menu_script.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_script : MonoBehaviour
+{
+
+    protected const float TRANSITION_DURATION = 2f;
+
+    protected float timer = 0f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LoadScene(string scene_name){
+        yield return null;
+
+        while(timer < TRANSITION_DURATION){
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        
+        AsyncOperation asyncload = SceneManager.LoadSceneAsync(scene_name);
+
+        while(!asyncload.isDone){
+            yield return null;
+        }
+    }
+
+    public void setACscene(){
+        StartCoroutine(LoadScene("AppleCatcher"));
+    }
+
+    public void setFBscene(){
+        StartCoroutine(LoadScene("FurapiBird"));
+    }
+
+    public void setBBscene(){
+        StartCoroutine(LoadScene("BrickBreaker"));
+    }
+
+}
